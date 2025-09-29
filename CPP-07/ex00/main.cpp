e3bc7d92ebeca800d2a9d8adcd0cdc56f4ef4af8
Leila <leilaticiane@gmail.com>
2025-09-29 20:30:15 +0100

    Refactor output statements in main function for clarity and consistency

--- CPP-07/ex00/main.cpp
+++ CPP-07/ex00/main.cpp
@@ -10,28 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "whatever.hpp"
 
 int main(void)
 {
+
 	int a = 2;
 	int b = 3;
-
-	std::cout << "[LOG] Before swap: a = " << a << ", b = " << b << std::endl;
 	::swap(a, b);
-	std::cout << "[LOG] After swap: a = " << a << ", b = " << b << std::endl;
-	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
-	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
-
-	std::string c = "xuxu1";
-	std::string d = "xuxu2";
-
-	std::cout << "[LOG] Before swap: c = " << c << ", d = " << d << std::endl;
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
 	::swap(c, d);
-	std::cout << "[LOG] After swap: c = " << c << ", d = " << d << std::endl;
-	std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
-	std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
-
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
 	return 0;
 }
